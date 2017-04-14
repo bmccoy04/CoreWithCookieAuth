0789b96ea3c0d035f40aa22524f5934292ab40c0
bryan <bryanmccoy04@gmail.com>
2017-04-14 12:33:19 -0500

    Initial Checking

---
+++ CoreWithCookieAuth.Site/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWithCookieAuth.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error() => View();
+    }
+}
---
+++ CoreWithCookieAuth.Site/Models/AccountViewModels/Login.cs
@@ -0,0 +1,9 @@
+namespace CoreWithCookieAuth.Site.Models.AccountViewModels
+{
+    public class Login
+    {
+        public string UserName { get; set; }    
+        public string Password { get; set; }
+                
+    }
+}
