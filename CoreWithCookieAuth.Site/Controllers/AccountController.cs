08cc908c17f160fd8c3aad87172f30b10acf85e4
bmccoy04 <bryanmccoy04@gmail.com>
2017-04-17 21:29:38 -0500

    Some name changes and form submit.

--- CoreWithCookieAuth.Site/Controllers/AccountController.cs
+++ CoreWithCookieAuth.Site/Controllers/AccountController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreWithCookieAuth.Site.Models.AccountViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreWithCookieAuth.Site.Controllers
@@ -10,7 +11,16 @@ namespace CoreWithCookieAuth.Site.Controllers
     {
         public IActionResult Index()
         {
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel ln){
+            if(ln.UserName == ln.Password)
+                return RedirectToAction("Index", "Home");
+            else
+                return View(new LoginViewModel(){UserName = "Bryan"});            
         }
     }
 }
--- CoreWithCookieAuth.Site/Models/AccountViewModels/LoginViewModel.cs
+++ CoreWithCookieAuth.Site/Models/AccountViewModels/LoginViewModel.cs
@@ -1,6 +1,6 @@
 namespace CoreWithCookieAuth.Site.Models.AccountViewModels
 {
-    public class Login
+    public class LoginViewModel
     {
         public string UserName { get; set; }    
         public string Password { get; set; }
